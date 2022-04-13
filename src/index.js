96d4d1ffb142f9d5ae05145202d71c660f10829e
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-13 16:01:26 +0200

    update index to react 18

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { render } from 'react-dom'
 import App from './App'
 import { createRoot } from 'react-dom/client'
 
 const container = document.getElementById('root')
-render(<App />, container)
 const root = createRoot(container)
 root.render(<App />)
