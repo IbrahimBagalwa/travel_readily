1534aa5c132ace21e6ded822a726581a1b6ce886
Ibrahim Bagalwa <bagmurhulaibrahim@gmail.com>
2022-04-13 18:50:01 +0200

    update

--- src/App.js
+++ src/App.js
@@ -12,18 +12,27 @@ function App() {
   const [bounds, setBounds] = useState(null)
   const theme = createTheme()
   useEffect(() => {
-    fetchPlacesData().then((data) => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude })
+      }
+    )
+  }, [])
+
+  useEffect(() => {
+    console.log(bounds, coordinates)
+    fetchPlacesData(bounds.sw, bounds.ne).then((data) => {
       console.log(data)
       setPlaces(data)
     })
-  }, [])
+  }, [coordinates, bounds])
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
